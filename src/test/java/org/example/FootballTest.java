56b88aaa38b421d19d98c9a75da7a6121f74a6ab
Tim M <timothy.romero.manas@gmail.com>
2023-11-02 20:43:51 -0400

    Extract Team names from response

--- src/test/java/org/example/FootballTest.java
+++ src/test/java/org/example/FootballTest.java
@@ -5,6 +5,8 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
@@ -63,6 +65,7 @@ public class FootballTest extends FootballConfig {
         System.out.println("ResponseBody: " + responseBody);
     }
 
+
     @Test
     public void extractAllTeamData_ThenValidate() {
         Response response = given()
@@ -73,7 +76,51 @@ public class FootballTest extends FootballConfig {
                     .extract().response();          // This extracts the response body
 
         System.out.println("response: " + response.asString());
+    }
+
+    @Test
+    public void extractHeaders() {
+        Response response = given()
+                .when()
+                .get("teams/57")
+                .then()
+                .contentType(ContentType.JSON)
+                .extract().response();
+
+        // Grab Headers
+        String headers = String.valueOf(response.getHeaders());
+        String header = response.getHeader("X-API-Version");
+        String contentTypeHeader = response.getContentType();
+        int statusCode = response.getStatusCode();
+
+
+        System.out.println("headers: " + headers);
+        System.out.println("header: " + header);
+        System.out.println("contentTypeHeader: " + contentTypeHeader);
+        System.out.println("statusCode: " + statusCode);
 
     }
 
+    // Fetch the first team name response using jsonPath
+    @Test
+    public void extractFirstTeamNameUsingJsonPath() {
+        String firstTeamName = get("competitions/2021/teams").jsonPath().getString("teams.name[0]");
+        String secondTeamName = get("competitions/2021/teams").jsonPath().getString("teams.name[1]");
+
+        System.out.println("firstTeamName: " + firstTeamName);
+        System.out.println("secondTeamName: " + secondTeamName);
+
+    }
+
+
+    @Test
+    public void extractAllTeamNames() {
+        // Get Response first
+        Response response = get("competitions/2021/teams").then()
+                .extract().response();
+
+        // Extract team names
+        List<String> teamNames = response.path("teams.name");           // this is equivalent to jsonPath
+        System.out.println("teamNames: " + teamNames);
+    }
 }
